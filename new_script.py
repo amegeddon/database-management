e4ee731c7e7b314b8cb9cc5742e24cf93f4cda81
amegeddon <ameescook@gmail.com>
2024-02-21 00:23:48 +0000

    Connects python file to database

--- new_script.py
+++ new_script.py
@@ -1,39 +1,35 @@
 import psycopg2
+from psycopg2 import OperationalError
 
+try:
+    # Connect to PostgreSQL
+    connection = psycopg2.connect(
+        dbname="chinook",
+        user="postgres",
+        password="waggies1",
+        host="localhost",
+        port="5432"
+    )
 
-# connect to "chinook" database
-connection = psycopg2.connect(database="chinook")
+    # Create a cursor object
+    cursor = connection.cursor()
 
-# build a cursor object of the database
-cursor = connection.cursor()
+    # Execute the query
+    cursor.execute('SELECT * FROM "track" WHERE "composer" = %s', ["Queen"])
 
-# Query 1 - select all records from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist"')
+    # Fetch the results
+    results = cursor.fetchall()
 
-# Query 2 - select only the "Name" column from the "Artist" table
-# cursor.execute('SELECT "Name" FROM "Artist"')
+    # Print results
+    for result in results:
+        print(result)
 
-# Query 3 - select only "Queen" from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+except OperationalError as e:
+    print("Error:", e)
 
-# Query 4 - select only by "ArtistId" #51 from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
-
-# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
-# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
-
-# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
-
-# fetch the results (multiple)
-results = cursor.fetchall()
-
-# fetch the result (single)
-# results = cursor.fetchone()
-
-# close the connection
-connection.close()
-
-# print results
-for result in results:
-    print(result)
+finally:
+    # Close the cursor and connection
+    if 'cursor' in locals():
+        cursor.close()
+    if 'connection' in locals():
+        connection.close()
