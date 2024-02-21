abab6ef998750a558dc049eddbc9b0e49d8ba393
amegeddon <ameescook@gmail.com>
2024-02-21 00:46:21 +0000

    Finishes adding queries to new_script.py

--- new_script.py
+++ new_script.py
@@ -15,10 +15,28 @@ try:
     cursor = connection.cursor()
 
     # Execute the query
+    # cursor.execute('SELECT * FROM "track" WHERE "composer" = %s', ["Queen"])
+
+    # Query 2 - select only the "Name" column from the "Artist" table
+    # cursor.execute('SELECT "name" FROM "artist"')
+    
+    # Query 3 - select only "Queen" from the "Artist" table
+    #cursor.execute('SELECT * FROM "artist" WHERE "name" = %s', ["Queen"])
+    # Fetch the results
+    
+    # Query 4 - select only by "ArtistId" #51 from the "Artist" table
+    # cursor.execute('SELECT * FROM "artist" WHERE "artist_id" = %s', [51])
+    
+    # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+    # cursor.execute('SELECT * FROM "album" WHERE "artist_id" = %s', [51])
+    
+    # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
     cursor.execute('SELECT * FROM "track" WHERE "composer" = %s', ["Queen"])
 
-    # Fetch the results
+    
     results = cursor.fetchall()
+    # fetch the result (single)
+    #results = cursor.fetchone()
 
     # Print results
     for result in results:
